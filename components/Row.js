07318f9dc5ebff9b289e5a2d92dd234cf62040f8
mendelB <mendelblesofsky@gmail.com>
2017-06-29 16:36:19 -0400

    Done.

--- components/Row.js
+++ components/Row.js
@@ -5,6 +5,16 @@ const Cell = require('./Cell');
 
 class Row extends React.Component {
   render () {
+  	const {row, onChange, onFocus} = this.props
+  	return (
+  		<tr className='row'>
+  			{
+  				row.map((cell, i)=> 
+  					<Cell key={i} cell={cell} onChange={onChange.bind(null, i)} onFocus={onFocus.bind(null, i)}/>
+  				)
+  			}
+  		</tr>
+  	)
   }
 }
 
--- components/Table.js
+++ components/Table.js
@@ -5,6 +5,18 @@ const Row = require('./Row');
 
 class Table extends React.Component {
   render () {
+  	let {table, onBlur, onChange, onFocus} = this.props
+  	return (
+  		<table className='table' onBlur={onBlur}>
+  			<tbody>
+  				{
+  					table.map((row, i) =>
+  						<Row key={i} row={row} onChange={onChange.bind(null, i)} onFocus={onFocus.bind(null, i)}/>
+  					)
+  				}
+  			</tbody>
+  		</table>
+  	)
   }
 }
 
